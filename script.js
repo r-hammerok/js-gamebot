7602aa57d912752263ded58c54d1c1e777740524
Roman Ivannikov <roman.y.ivannikov@gmail.com>
2021-02-06 07:46:17 +0700

    build: completed lesson 6 normal

---
+++ script.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const isNumber = function(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+const game = function() {
+  // Число, загаданное ботом
+  const botNumber = Math.ceil(Math.random() * 99);
+  // Знать, какое число бот загадал
+  console.log('Бот загадал: ' + botNumber);
+
+  const geussing = function() {
+    const answer = prompt('Угадай число от 1 до 100');
+
+    if (answer === null) {
+      alert('Игра закончена');
+      return;
+    }
+
+    if (!isNumber(answer)) {
+      alert('Введи число!');
+      geussing();
+    }
+
+    if (+answer > botNumber) {
+      alert('Загаданное число меньше');
+      geussing();
+    } else if (+answer < botNumber) {
+      alert('Загаданное число больше');
+      geussing();
+    } else if (+answer === botNumber) {
+      alert('Поздравляю, Вы угадали!!!');
+      return;
+    }
+
+  };
+
+  geussing();  
+
+};
+
+game();
+
