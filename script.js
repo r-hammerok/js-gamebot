681870fd7cdf967409c5b0c30f4ed321c353155e
Roman Ivannikov <roman.y.ivannikov@gmail.com>
2021-02-06 10:51:33 +0700

    fix(script.js): переделал алгоритм, убрал цикл

--- script.js
+++ script.js
@@ -6,21 +6,18 @@ const isNumber = function(n) {
 
 const game = function() {
   // Число, загаданное ботом
-  let botNumber;
-
+  const botNumber = Math.ceil(Math.random() * 99);
+  // Знать, какое число бот загадал
+  console.log('Бот загадал: ' + botNumber);
   // Максимальное количество попыток
-  let maxAttempts = 10;
-
-  // Причина выхода из рекурсии
-  // 1 - закончились попытки
-  // 2 - отгадал число
-  // 0 - нажал Отмена
-  let whyReturn = 0;
+  const maxAttempts = 10;
 
   const guessing = function(attempts) {
 
     if (attempts === 0) {
-      whyReturn = 1;
+      if (confirm('Попытки закончились, хотите сыграть еще?')) {
+        game();
+      }
       return;
     }
 
@@ -28,7 +25,6 @@ const game = function() {
 
     if (answer === null) {
       alert('Игра закончена');
-      whyReturn = 0;
       return;
     }
 
@@ -44,34 +40,16 @@ const game = function() {
       alert('Загаданное число больше, осталось попыток ' + --attempts);
       guessing(attempts);
     } else if (+answer === botNumber) {
-      whyReturn = 2;
+      if (confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?')) {
+        game();
+      }
       return;
     }
 
     return;
   };
 
-  let anew;
-  
-  do {
-    botNumber = Math.ceil(Math.random() * 99);
-    // Знать, какое число бот загадал
-    console.log('Бот загадал: ' + botNumber);
-    guessing(maxAttempts);
-
-    switch (whyReturn) {
-      case 1:
-        anew = confirm('Попытки закончились, хотите сыграть еще?');
-        break;
-      case 2:
-        anew = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
-        break;
-      default:
-        anew = false;
-    }
-    
-  } while (anew);
-
+  guessing(maxAttempts);
 };
 
 game();
